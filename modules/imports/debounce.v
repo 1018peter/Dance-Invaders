a1aaeefe4fedaaf5ea96bc6fa5101e61b477f437
1018peter <56677134+1018peter@users.noreply.github.com>
2020-12-26 20:46:34 +0800

    Add files via upload

---
+++ modules/imports/debounce.v
@@ -0,0 +1,35 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/12/24 21:10:32
+// Design Name: 
+// Module Name: debounce
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module debounce(pb_debounced, pb ,clk);
+    output pb_debounced;
+    input pb;
+    input clk;
+    
+    reg [6:0] shift_reg;
+    always @(posedge clk) begin
+        shift_reg[6:1] <= shift_reg[5:0];
+        shift_reg[0] <= pb;
+    end
+    
+    assign pb_debounced = shift_reg == 7'b111_1111 ? 1'b1 : 1'b0;
+endmodule
