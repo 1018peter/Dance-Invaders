403e574d149f9d7e707e4ca0a356c7511d2fef51
gg-blog <67037449+gg-blog@users.noreply.github.com>
2020-12-28 00:45:40 +0800

    Add files via upload
    the version after solving reset bug and renew header and odd parity.

--- data_transmission/receiver.v
+++ data_transmission/receiver.v
@@ -1,35 +1,58 @@
-module receiver#(parameter n = 6)  //four phase handshaking receiver,parameter means the number of total bits per unit data. 
+module receiver#(parameter n = 1500)  //four phase handshaking receiver,parameter means the total bits  to transfer. 
                (input clk_receiver, //clock domain of the receiver.
                 input wire_req,   //require signal from sender
                 input [5:0] wire_data_deliver, //data of delivering
-                input rst,  //reset
                 output [n-1:0] wire_data_out, //unit data you want to transfer.
                 output reg reg_ack, //acknowlege signal from receiver.
                 output reg reg_valid); //Is the data enable to use? 1 means yes,0 means no.
-    parameter m=n+6-n%6;
-    reg [7:0] reg_pointer;//pointer of the beginning of where 6 bit data should transfer
-    reg reg_ready;//ready to receive?1:0;
-    reg [m-1:0] reg_data_result;//enough space to store output.
+    parameter m=n+4-n%4;
+    reg [10:0] reg_pointer=0;//pointer of the beginning of where 4 bit data should transfer
+    reg reg_ready=1;//ready to receive?1:0;
+    reg [m-1:0] reg_data_result=0;//enough space to store output.
+    reg reg_header_receive=0;//receive header?1:0;
     integer i;
-    always @(posedge clk_receiver or posedge rst) begin
-        if(rst) begin
-            reg_ack   <= 0;
-            reg_ready <= 1;
-            reg_data_result <=0;
-            reg_pointer <= 0;
-            reg_valid <= 0;
-        end 
-        else begin   
-        if (wire_req == 1) begin
+    always @(posedge clk_receiver) begin
+        if (wire_req == 1'b1) begin
+            if(wire_data_deliver[0]^wire_data_deliver[1]^wire_data_deliver[2]^wire_data_deliver[3]^wire_data_deliver[4]^wire_data_deliver[5]==1) begin
             reg_ack   <= 1;
-            reg_ready <= 0;
-            for(i = 0;i<6;i = i+1) begin
+            reg_ready <=0;
+            if(reg_header_receive==1'b1) begin
+                for(i = 0;i<4;i = i+1) begin
                 if (reg_pointer+i<n) 
                     reg_data_result[reg_pointer+i]      <= wire_data_deliver[i];
                 else reg_data_result[reg_pointer+i] <= 0;
+                end
+                if(wire_data_deliver[4]==1'b1)  begin
+                    reg_pointer <=0;
+                    reg_header_receive <= 0;
+                    reg_ready <= 0;
+                end
+                else begin
+                    reg_pointer <= reg_pointer;
+                    reg_header_receive<=reg_header_receive;
+                    reg_ready <= 0;
+                end
             end
-            reg_pointer <= reg_pointer;
+            else begin
+                    reg_pointer <= 0;
+                    reg_header_receive<=0;
+                    reg_ready <= 0;
+                reg_data_result<=0;
+            end
             reg_valid <= 0;
+            end
+            else begin
+                reg_ack <=0;
+                reg_ready <=0;
+                reg_header_receive <=reg_header_receive;
+                for(i = 0;i<4;i = i+1) begin
+                if (reg_pointer+i<n) 
+                    reg_data_result[reg_pointer+i]      <= wire_data_deliver[i];
+                else reg_data_result[reg_pointer+i] <= 0;
+                end
+                reg_pointer <= reg_pointer;
+                reg_valid <= 0;
+            end
         end
         else begin
             reg_ack <= 0;
@@ -37,27 +60,29 @@ module receiver#(parameter n = 6)  //four phase handshaking receiver,parameter m
                 reg_ready   <= 1;
                 reg_valid <= reg_valid;
                 reg_pointer <= reg_pointer;
-                for(i = 0;i<6;i = i+1) begin
-                    if (reg_pointer+i<n)
-                       reg_data_result[reg_pointer+i] <= wire_data_deliver[i];
-                    else reg_data_result[reg_pointer+i] <= 0;
-                end
+                reg_data_result <=reg_data_result;
+                reg_header_receive <= reg_header_receive;
+
             end
             else begin
             reg_ready <= 1;
-            for(i = 0;i<6;i = i+1) begin
-                reg_data_result[i] <= wire_data_deliver[i];
-            end
-            if (reg_pointer+6<n) begin
-                reg_pointer      <= reg_pointer+6;
+            reg_data_result<=reg_data_result;
+            if (reg_pointer+4<n&&reg_header_receive==1) begin
+                reg_pointer      <= reg_pointer+4;
                 reg_valid <= 0;
+                reg_header_receive<=reg_header_receive;
             end
-            else  begin
+            else if(reg_header_receive==0)begin
+                reg_pointer <= 0;
+                reg_valid <=0;
+                reg_header_receive<=1;
+            end
+            else begin
                 reg_pointer <= 0;
                 reg_valid <=1;
+                reg_header_receive<=0;
             end
             end
-        end
         end
     end
     assign wire_data_out=reg_data_result[n-1:0];               
--- data_transmission/sender.v
+++ data_transmission/sender.v
@@ -1,74 +1,99 @@
-module sender #(parameter n = 6)  //four phase handshaking sender,parameter means the number of total bits per unit data. 
+module sender #(parameter n = 1500)  //four phase handshaking sender,parameter means the total bits you may transfer.
                (input clk_sender, //clock domain of the sender.
                 input wire_ack,   //acknowledge signal from receiver
                 input[n-1:0] wire_data_in, //unit data you want to transfer.
-                input wire_write_en,       //enable to transfer? 1 means yes,0 means no.
                 input rst,   //reset
                 output reg [5:0] reg_data_out, //data to deliver
                 output reg reg_req); //require signal from sender.
-    reg [7:0] reg_pointer;//pointer of the beginning of where 6 bit data should transfer
+    reg [10:0] reg_pointer;//pointer of the beginning of where 4 bit data should transfer
     reg [n-1:0] reg_data_register;//buffer to store data until transmission finish.
     reg reg_ready;//ready to send?1:0;
+    reg reg_header;//has header been sent?1:0;
+    reg reg_xor;//just for xor operation for odd parity.
+    reg reg_rst;//record rst signal to transfer to the receiver.
+    reg [3:0] reg_xor_register;//just for xor operation for odd parity.
     integer i;
-    always @(posedge clk_sender or posedge rst) begin
-        if(rst) begin
-            reg_req   <= 0;
-            reg_ready <= 1;
-            reg_data_out <=0;
-            reg_pointer <= 0;
+    always @* begin
+        if(reg_header==1) begin
+            for(i = 0;i<4;i = i+1) begin
+                if (reg_pointer+i<n) 
+                    reg_xor_register[i]      <= reg_data_register[reg_pointer+i];
+                else reg_xor_register[i]    <= 0;
+            end
         end
         else begin
-        if (wire_write_en) begin
-            if (wire_ack == 0) begin
+            reg_xor_register[3:0]<=4'd0;
+        end
+        reg_xor=reg_xor_register[0]^reg_xor_register[1]^reg_xor_register[2]^reg_xor_register[3]^reg_rst^1;
+    end
+    always @(posedge clk_sender or posedge rst) begin
+        if(rst) begin
+            reg_req   <= 1'b0;
+            reg_ready <= 1;
+            reg_data_out <=6'b010000;
+            reg_pointer <= 0;
+            reg_header <=0;
+            reg_rst <= 1;
+        end
+        else begin
+            if (wire_ack == 1'b0) begin
                 reg_req   <= 1;
                 reg_ready <= 0;
-                for(i = 0;i<6;i = i+1) begin
+                reg_rst <= reg_rst;
+                reg_header <= reg_header;
+                if(reg_header==1) begin
+                for(i = 0;i<4;i = i+1) begin
                     if (reg_pointer+i<n) 
                         reg_data_out[i]      <= reg_data_register[reg_pointer+i];
                     else reg_data_out[i]    <= 0;
                 end
+                end
+                else begin
+                    reg_data_out[3:0]<=4'd0;
+                end
+                reg_data_out[4]<=reg_rst;
+                reg_data_out[5] <= reg_xor;
                 reg_pointer <= reg_pointer;
             end
             else begin
                 reg_req <= 0;
+                reg_rst <=0;
                 if (reg_ready) begin
-                    reg_ready   <= 1;
-                    reg_pointer <= reg_pointer;
-                    for(i = 0;i<6;i = i+1) begin
-                        if (reg_pointer+i<n)
-                           reg_data_out[i]      <= reg_data_register[reg_pointer+i];
-                        else reg_data_out[i]    <= 0;
-                   end
+                reg_ready   <= 1;
+                reg_pointer <= reg_pointer;
+                reg_header <=reg_header;
+                for(i = 0;i<6;i = i+1) begin
+                    reg_data_out[i] <= reg_data_out[i];
+                end
                 end
                 else begin
                 reg_ready <= 1;
                 for(i = 0;i<6;i = i+1) begin
-                    reg_data_out[i] <= reg_data_register[i];
+                    reg_data_out[i] <= reg_data_out[i];
                 end
-                if (reg_pointer+6<n)
-                    reg_pointer      <= reg_pointer+6;
-                else
+                if (reg_pointer+4<n&&reg_header==1) begin
+                    reg_pointer      <= reg_pointer+4;
+                    reg_header <= 1;
+                end
+                else if(reg_header==0) begin
+                    reg_header <= 1;
                     reg_pointer <= 0;
                 end
+                else begin
+                    reg_header <= 0;
+                    reg_pointer <= 0;
+                end
+                end
             end
-        end
-        else begin
-            reg_req   <= 0;
-            reg_ready   <= 1;
-            reg_pointer <= 0;
-            for(i = 0;i<6;i = i+1) begin
-                reg_data_out[i] <= reg_data_register[i];
-            end
-        end
         end
     end
 
     always @(posedge clk_sender or posedge rst) begin
         if(rst) begin
-            reg_data_register <= wire_data_in;
+            reg_data_register <= 0;
         end
         else begin
-        if(reg_pointer+6>=n)begin
+        if(reg_pointer==0&&reg_header==0)begin
             reg_data_register <= wire_data_in;
         end
         else begin
