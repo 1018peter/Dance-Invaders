05d9ac0c96e5b06871fdd3b431999b7307004080
1018peter <56677134+1018peter@users.noreply.github.com>
2021-01-10 20:20:48 +0800

    Added background image and memory.

--- core_modules/layer_background.sv
+++ core_modules/layer_background.sv
@@ -30,6 +30,22 @@ module layer_background(
     // TODO: Instantiate a memory block for the (sparse?) background using its .coe form.
     
     // Default.
-    assign pixel = 12'h0_0_0;
+    wire [15:0] pixel_addr = (v_cnt >> 1) * 4 * 240 + (h_cnt >> 1);
+    wire [7:0] hex_bus;
+    wire [3:0] palette_select;
+    assign palette_select = pixel_addr[0] ? hex_bus[7:4] : hex_bus[3:0];
+    
+    parameter [11:0] PALETTE_COLOR = {
+    12'h122, 12'h112, 12'h112, 12'haaa,
+    12'h122, 12'h122, 12'h111, 12'h112,
+    12'h222, 12'h112, 12'h112, 12'h112,
+    12'h112, 12'h112, 12'h112, 12'h112
+    };
+    assign pixel = PALETTE_COLOR[palette_select];
+    background_block_mem(
+    .clka(clk),
+    .addra({pixel_addr[15:4], 3'b0}),
+    .douta(hex_bus)
+    );
     
 endmodule
